536c41e0a3f910f00fa1c84eb853f65a77fe09b9
Hariz <syedhariz1995@gmail.com>
2023-05-06 18:35:08 +0800

    sidebar added

---
+++ components/Sidebar.js
@@ -0,0 +1,41 @@
+import {
+  HomeIcon,
+  HashtagIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  BellIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+} from "@heroicons/react/outline";
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div className="h-full hidden md:flex flex-col fixed xl:ml-24">
+      <nav className="h-full relative xl:space-y-1.5">
+      <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
+        <Image src={"/assets/twitter-logo.png"} width={34} height={34} />
+      </div>
+        <SidebarLink Icon={HomeIcon} text={"Home"} />
+        <SidebarLink Icon={HashtagIcon} text={"Explore"} />
+        <SidebarLink Icon={BellIcon} text={"Notifications"} />
+        <SidebarLink Icon={InboxIcon} text={"Messages"} />
+        <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
+        {/* <SidebarLink Icon={HomeIcon} text={"Twitter Blue"}/> */}
+        <SidebarLink Icon={UserIcon} text={"Profile"} />
+        <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+      <div className="absolute bottom-0">User</div>
+      </nav>
+    </div>
+  );
+}
+
+function SidebarLink({ text, Icon }) {
+  return (
+    <li className=" hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </li>
+  );
+}
